c1605809c5e99995aa325bd55d62d94925d1b69f
kartinich <dkostrikin@inbox.ru>
2020-09-25 17:43:53 +0300

    time zone

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,8 @@ module Bbq
 
     config.i18n.default_locale = :ru
 
+    config.time_zone = 'Moscow'
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
