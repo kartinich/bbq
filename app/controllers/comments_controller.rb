08cbd13e24d1c59eafc4113beb413d1373766dab
kartinich <dkostrikin@inbox.ru>
2020-11-03 15:08:00 +0300

    do not send your own comment message

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -45,6 +45,10 @@ class CommentsController < ApplicationController
     # Собираем всех подписчиков и автора события в массив мэйлов, исключаем повторяющиеся
     all_emails = (event.subscriptions.map(&:user_email) + [event.user.email]).uniq
 
+    if comment.user
+      all_emails.delete(comment.user.email)
+    end
+
     # По адресам из этого массива делаем рассылку
     # Как и в подписках, берём EventMailer и его метод comment с параметрами
     # И отсылаем в том же потоке
