437b24007b64f5e6d3d2706041b627a556966fe8
kartinich <dkostrikin@inbox.ru>
2020-11-11 15:00:28 +0300

    error 500 fixed

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,8 +1,7 @@
 class Photo < ApplicationRecord
   belongs_to :event
-  belongs_to :user, optional: true
+  belongs_to :user
 
-  validates :event, presence: true
   validates :user,  presence: true
   validates :photo, presence: true
 
